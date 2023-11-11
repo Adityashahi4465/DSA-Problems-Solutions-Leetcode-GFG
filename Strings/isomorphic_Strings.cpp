b1ed303ac8990ccb5c9030def037840abf2df023
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2023-11-11 19:15:55 +0530

    Create isomorphic_Strings.cpp

---
+++ Strings/isomorphic_Strings.cpp
@@ -0,0 +1,21 @@
+// https://www.geeksforgeeks.org/problems/isomorphic-strings-1587115620/1
+class Solution
+{
+    public:
+    //Function to check if two strings are isomorphic.
+    bool areIsomorphic(string str1, string str2)
+    {
+        
+        // Your code here
+        if(str1.size() != str2.size()) return false;
+        int m1[256] = {0}, m2[256] = {0};
+        
+        for(int i=0; i<str1.size(); ++i){
+            if(m1[str1[i]] != m2[str2[i]]) return false;
+            
+            m1[str1[i]] = i+1;
+            m2[str2[i]] = i+1;
+        }
+        return true;
+    }
+};
