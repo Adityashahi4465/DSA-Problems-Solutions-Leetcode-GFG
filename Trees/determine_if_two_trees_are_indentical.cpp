fe56e099d5afa34a69076b77d3df576a15a1cd05
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2023-11-21 21:22:06 +0530

    Create determine_if_two_trees_are_indentical.cpp

---
+++ Trees/determine_if_two_trees_are_indentical.cpp
@@ -0,0 +1,26 @@
+// https://www.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1
+
+  bool ans = true;
+    void inorder(Node *r1, Node *r2){
+        if(!r1){
+            if(r2)
+                ans = false;
+            return;
+        }
+        if(!r2){
+            if(r1)
+                ans = false;
+            return;
+        }
+        inorder(r1->left, r2->left);
+        if(r1->data != r2->data)
+            ans = false;
+        inorder(r1->right, r2->right);
+    }
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        //Your Code here
+        inorder(r1, r2);
+        return ans;
+    }
