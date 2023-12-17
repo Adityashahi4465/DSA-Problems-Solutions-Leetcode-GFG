3f45059b018a14955013224dfb3700366956d73b
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2023-12-17 23:04:41 +0530

    Create sum_with_without_adjecents.cpp

---
+++ DP/sum_with_without_adjecents.cpp
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/problems/max-sum-without-adjacents2430/1
+
+int dynamic( int arr[], int n, vector<int>&dp)
+	{
+	    //base conditon
+	    if(n<0)
+	    return 0;
+	    
+	    if(n==0)
+	    return arr[0];
+	    
+	    
+	    if(dp[n]!=-1)
+	    return dp[n];
+	    
+	    int pick = arr[n]+ dynamic(arr , n-2, dp);
+	    
+	    int notpick = 0+ dynamic(arr,n-1,dp);
+	    
+	    return dp[n] = max(pick ,notpick);
+	    
+	}
+	int findMaxSum(int *arr, int n) {
+	    // code here
+	    
+	    vector<int>dp(n+1,-1);
+	    
+	     return dynamic(arr, n-1 ,dp);
+	}
