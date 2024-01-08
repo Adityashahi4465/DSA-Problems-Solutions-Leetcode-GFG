076777f8fec28940e41a7d01bd2e896175ba788d
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2024-01-08 21:57:58 +0530

    Create range_sum_of_bst.cpp

---
+++ Trees/range_sum_of_bst.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/range-sum-of-bst/description/?envType=daily-question&envId=2024-01-08
+
+int rangeSumBST(TreeNode* root, int L, int R) {
+    if (!root) {
+        return 0;
+    }
+
+    if (root->val < L) {
+        return rangeSumBST(root->right, L, R);
+    }
+
+    if (root->val > R) {
+        return rangeSumBST(root->left, L, R);
+    }
+
+    return root->val + rangeSumBST(root->left, L, R) + rangeSumBST(root->right, L, R);
+}
