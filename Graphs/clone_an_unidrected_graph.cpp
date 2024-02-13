63c4a055ccb31a381ade2e2231e5b67d19f78484
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2024-02-13 23:52:35 +0530

    Create clone_an_unidrected_graph.cpp

---
+++ Graphs/clone_an_unidrected_graph.cpp
@@ -0,0 +1,19 @@
+// https://www.geeksforgeeks.org/problems/clone-graph/1
+
+Node* help(Node* node,vector<Node*>& visited)
+    {
+        Node* graph=new Node(node->val);
+        visited[node->val]=graph;
+        for(auto it:node->neighbors)
+        {
+            if(!visited[it->val])
+            help(it,visited);
+            
+            graph->neighbors.push_back(visited[it->val]);
+        }
+        return graph;
+    }
+    Node* cloneGraph(Node* node) {
+        vector<Node*> visited(10001,NULL);
+        return help(node,visited);
+    }
