0329bfd08cbe849f9c10231d4b6592a3b92d5b78
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2024-02-15 23:23:45 +0530

    Create count_akk_possible_paths.cpp

---
+++ Graphs/count_akk_possible_paths.cpp
@@ -0,0 +1,15 @@
+
+// https://www.geeksforgeeks.org/problems/castle-run3644/1
+
+int isPossible(vector<vector<int>>paths){
+	    for(int i=0;i<paths.size();i++)
+	    {
+	        int cnt=0;
+	        for(int j=0;j<paths[0].size();j++)
+	        cnt+=paths[i][j];
+	        
+	        if(cnt&1)
+	        return 0;
+	    }
+	    return 1;
+	}
