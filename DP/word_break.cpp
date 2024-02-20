7285c12aa3f9b7a08e8973a54319b020cfb395f3
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2024-02-20 23:58:28 +0530

    Create word_break.cpp

---
+++ DP/word_break.cpp
@@ -0,0 +1,20 @@
+// https://www.geeksforgeeks.org/problems/word-break1352/1
+
+int wordBreak(int n, string s, vector<string> &dictionary) {
+        //code here
+        int l=s.length();
+        vector<int>dp(l+1,0);
+        dp[0]=1;
+        for(int i=0;i<=l;i++)
+        {
+            for(string& word:dictionary){
+                int start=i-word.length();
+                if (start >= 0 && dp[start] == 1 && s.substr(start, word.length()) == word) {
+                dp[i] = 1;
+                break;
+                }
+            }
+        }
+        return dp[l];
+    }
+
