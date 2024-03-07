ff79ecd71d433f2a32b74c18d88ea6d7a63290f6
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2024-03-07 22:37:27 +0530

    Create longest_repeating_and_non_overlapping_substring.cpp

---
+++ DP/longest_repeating_and_non_overlapping_substring.cpp
@@ -0,0 +1,39 @@
+// https://www.geeksforgeeks.org/problems/longest-repeating-and-non-overlapping-substring3421/1
+
+string longestSubstring(string s, int n)
+    {
+        // code here
+
+        int left = 0;
+
+        string temp = "";
+        string result;
+
+        bool flag = true;
+        
+        int ans = -1;
+        for (int right = 0; right < n; right++)
+        {
+            temp += s[right];
+
+            if (s.find(temp, right + 1) != -1)
+            {
+                flag = false;
+                if (right - left + 1 > ans)
+                {
+                    ans = right - left + 1;
+                    result = temp;
+                }
+            }
+            else
+            {
+                temp.erase(temp.begin());
+                left++;
+            }
+        }
+        
+        if(flag){
+            return "-1";
+        }
+        return result;
+    }
